fea1cbefe5a5158acdb03c04d499457585971200
xLychee <xun.luan.rice@gmail.com>
2017-10-18 11:29:42 -0500

    Change Action set to [(1,),(2,),(3,)].

--- Config.py
+++ Config.py
@@ -23,6 +23,10 @@
 # OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
 
 class Config:
     #########################################################################
