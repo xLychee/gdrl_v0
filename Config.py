9ef2f9e113fafccbbbf241360a1737bb849447f7
xLychee <xun.luan.rice@gmail.com>
2017-10-18 16:31:23 -0500

    Enlarged Action set.

--- Config.py
+++ Config.py
@@ -33,7 +33,8 @@ class Config:
     # Game configuration
 
     # Name of the game, with version (e.g. PongDeterministic-v0)
-    ATARI_GAME = 'PongDeterministic-v0'
+    #ATARI_GAME = 'PongDeterministic-v0'
+    ATARI_GAME = 'Pong-v0'
 
     # Enable to see the trained agent in action
     PLAY_MODE = False
