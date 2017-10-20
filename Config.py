f04814f7c87cc24e116476b61591d43db5a91ffe
xLychee <xun.luan.rice@gmail.com>
2017-10-19 22:36:27 -0500

    QbertDeterministic.

--- Config.py
+++ Config.py
@@ -37,7 +37,8 @@ class Config:
 
 
     # ATARI_GAME = 'PongDeterministic-v0'
-    ATARI_GAME = 'QbertDeterministic-v0'
+    # ATARI_GAME = 'QbertDeterministic-v0'
+    ATARI_GAME = 'BreakoutDeterministic-v0'
 
     # Enable to see the trained agent in action
     PLAY_MODE = False
@@ -149,6 +150,7 @@ class Config:
     USE_LOG_SOFTMAX = False
 
     #########################################################################
+    '''
     BASIC_ACTION_SET = [0, 1, 2, 3, 4, 5]  # (1, 1), (1, 2), (1, 3), (2, 1), (2, 2), (2, 3), (3, 1), (3, 2), (3, 3)]
     ENLARGED_ACTION_SET = [(0,), (1,), (2,), (3,), (4,), (5,),
                            (0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5),
@@ -157,6 +159,13 @@ class Config:
                            (3, 0), (3, 1), (3, 2), (3, 3), (3, 4), (3, 5),
                            (4, 0), (4, 1), (4, 2), (4, 3), (4, 4), (4, 5),
                            (5, 0), (5, 1), (5, 2), (5, 3), (5, 4), (5, 5)]
+    '''
+    BASIC_ACTION_SET = [0, 1, 2, 3]  # (1, 1), (1, 2), (1, 3), (2, 1), (2, 2), (2, 3), (3, 1), (3, 2), (3, 3)]
+    ENLARGED_ACTION_SET = [(0,), (1,), (2,), (3,),
+                           (0, 0), (0, 1), (0, 2), (0, 3),
+                           (1, 0), (1, 1), (1, 2), (1, 3),
+                           (2, 0), (2, 1), (2, 2), (2, 3),
+                           (3, 0), (3, 1), (3, 2), (3, 3)]
 
     def build_action_index_map(action_set):
         index = 0
